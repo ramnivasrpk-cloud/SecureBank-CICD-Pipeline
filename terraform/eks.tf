4b22771395ab8c854a59ca12c6606b3f4a3f2411
ramnivas <ramnivasrpk@gmail.com>
2026-04-22 10:04:04 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,23 +1,36 @@
-resource "aws_eks_cluster" "securebank_eks" {
-  name     = "securebank-eks-cluster"
-  role_arn = aws_iam_role.eks_role.arn
-
-  vpc_config {
-    subnet_ids = []
-  }
-}
-
-resource "aws_iam_role" "eks_role" {
-  name = "securebank-eks-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.21.0"
+ 
+  cluster_name    = "securebank-eks-cluster"
+  cluster_version = "1.28"
+ 
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+ 
+  eks_managed_node_groups = {
+    banking_nodes = {
+      instance_types = ["t3.large"]
+      min_size       = 3
+      max_size       = 8
+      desired_size   = 3
+      disk_size      = 50
+      labels = {
+        role = "banking-app"
       }
-    }]
-  })
+      taints = []
+    }
+  }
+ 
+  # Enable IRSA (IAM Roles for Service Accounts)
+  enable_irsa = true
+ 
+  tags = { Name = "securebank-eks-cluster" }
+}
+ 
+# IAM role for Jenkins to access EKS
+resource "aws_iam_role_policy_attachment" "jenkins_eks" {
+  role       = aws_iam_role.jenkins.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
